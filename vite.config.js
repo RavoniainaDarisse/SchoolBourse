6589eccc3ecf1f9e658a80284daf93ee484ca785
RavoniainaDarisse <ravoniainaheritiana@gmail.com>
2025-12-19 09:33:00 +0300

    build

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,19 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import path from "path"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // 🔴 OBLIGATOIRE pour Netlify
+  base: "/",
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
+  // ⚠️ server = DEV uniquement (Netlify ne l'utilise pas)
   server: {
     host: "0.0.0.0",
     port: 5173,
